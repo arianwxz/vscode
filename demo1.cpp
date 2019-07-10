0759a8d858f6bbcbafff3b9a8e0d8ba2201284de
wxz <765007340@qq.com>
2019-07-10 20:58:11 +0800

    first time

---
+++ demo1.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+int case(int a)
+{
+    int bs;
+    bs=a;
+    return bs;
+}
+int main()
+{
+    int b;
+    int bc;
+}
---
+++ demo_c++/compare.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int seea(int a)
+{
+   int b=a;
+   return b;
+}
+int demo(int b)
+{
+   return b;
+}
+int main()
+{
+   int a=10;
+   cout<<seea(a);
+   system("pause");
+}
