91d21597fa9931ec0dbaf08f9e91484d653d8c52
jason <seu_arianwxz@163.com>
2019-12-07 17:59:08 +0800

    version

---
+++ demo_c++/Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+class A{
+    int a;
+};
+
+int main()
+{
+
+    A &a=new A();
+}
--- demo_c++/test.cpp
+++ demo_c++/test.cpp
@@ -1,15 +1,90 @@
 #include<iostream>
-#include<thread>
-#include <chrono>
+#include<string>
 using namespace std;
+
+//template<class T>
+
+class People
+{
+public:
+    string name,year,sex,address,tel;
+    People(string Name,string Year,string Sex,string Address,string Tel);
+    ~People();
+};
+People::People(string Name,string Year,string Sex,string Address,string Tel)
+{
+    this->name=Name;
+    this->year=Year;
+    this->sex=Sex;
+    this->address=Address;
+    this->tel=Tel;
+}
+People::~People()
+{
+
+}
+class Teacher:virtual public People
+{
+private:
+public:
+    string title;
+    Teacher(string Name,string Year,string Sex,string Address,string Tel,string Title);
+    ~Teacher();
+};
+
+Teacher::Teacher(string Name,string Year,string Sex,string Address,string Tel,string Title):People(Name,Year,Sex,Address,Tel),title(Title)
+{
+}
+
+Teacher::~Teacher()
+{
+}
+
+class Cadre:virtual public People
+{
+private:
+public:
+    string post;
+    Cadre(string Name,string Year,string Sex,string Address,string Tel,string Post);
+    ~Cadre();
+};
+
+Cadre::Cadre(string Name,string Year,string Sex,string Address,string Tel,string Post):People(Name,Year,Sex,Address,Tel),post(Post)
+{
+}
+
+Cadre::~Cadre()
+{
+}
+
+class Teacher_Cadre: public Teacher,virtual public Cadre 
+{
+private:
+public:
+    string wages;
+    friend ostream & operator <<(ostream & , Teacher_Cadre &);
+    Teacher_Cadre(string Name,string Year,string Sex,string Address,string Tel,string Title,string Post,string Wages);
+    ~Teacher_Cadre();
+};
+
+Teacher_Cadre::Teacher_Cadre(string Name,string Year,string Sex,string Address,string Tel,string Title,string Post,string Wages):People(Name,Year,Sex,Address,Tel),Teacher(Name,Year,Sex,Address,Tel,Title),Cadre(Name,Year,Sex,Address,Tel,Post),wages(Wages)
+{
+}
+
+Teacher_Cadre::~Teacher_Cadre ()
+{
+}
+
+ostream & operator <<(ostream & os,const Teacher_Cadre & c)
+{
+    os << c.name << " " << c.year << " "<<c.sex<<" "<<c.title<<" "<<c.address<<" "<<c.tel<<" "<<c.post<<" "<<c.wages<<endl; 
+    return os;
+}
 
 int main()
 {
-    int a[4]={1,2,3,4};
-    for (auto b:a)
-    {
-        cout<<b<<" ";
-    }
-    system("pause");
-    
+ 
+ Teacher_Cadre person("jason","27","B","Nanjing","12345213","porfessor","monitor","20000");
+// cout<<person;
+ system("pause");
 }
